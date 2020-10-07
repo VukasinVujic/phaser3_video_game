ceb1500d0a9f5face6b98ecabf61ec925c79e88e
VukasinVujic <vukasinvujic021@gmail.com>
2020-10-07 13:31:09 +0200

    Change from image to sprite

--- Scene1.js
+++ Scene1.js
@@ -4,9 +4,29 @@ class Scene1 extends Phaser.Scene {
   }
   preload() {
     this.load.image("background", "assets/images/background.png");
-    this.load.image("ship1", "assets/images/ship.png");
-    this.load.image("ship2", "assets/images/ship2.png");
-    this.load.image("ship3", "assets/images/ship3.png");
+    // this.load.image("ship1", "assets/images/ship.png");
+    // this.load.image("ship2", "assets/images/ship2.png");
+    // this.load.image("ship3", "assets/images/ship3.png");
+
+    this.load.spritesheet("ship", "assets/spritesheetes/ship.png", {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
+
+    this.load.spritesheet("ship2", "assets/spritesheetes/ship2.png", {
+      frameWidth: 32,
+      frameHeight: 16,
+    });
+
+    this.load.spritesheet("ship3", "assets/spritesheetes/ship3.png", {
+      frameWidth: 32,
+      frameHeight: 32,
+    });
+
+    this.load.spritesheet("explosion", "assets/spritesheetes/explosion.png", {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
   }
   create() {
     this.add.text(20, 20, "Loading game...");
--- Scene2.js
+++ Scene2.js
@@ -17,19 +17,54 @@ class Scene2 extends Phaser.Scene {
     );
     this.background.setOrigin(0, 0);
 
-    this.ship1 = this.add.image(
+    // change image to sprite
+    this.ship1 = this.add.sprite(
       config.width / 2 - 50,
       config.height / 2,
-      "ship1"
+      "ship"
     );
+    // change image to sprite
+    this.ship2 = this.add.sprite(config.width / 2, config.height / 2, "ship2");
 
-    this.ship2 = this.add.image(config.width / 2, config.height / 2, "ship2");
-
-    this.ship3 = this.add.image(
+    // change image to sprite
+    this.ship3 = this.add.sprite(
       config.width / 2 + 50,
       config.height / 2,
       "ship3"
     );
+
+    this.anims.create({
+      key: "ship1_anim",
+      frames: this.anims.generateFrameNumbers("ship"),
+      frameRate: 20,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "ship2_anim",
+      frames: this.anims.generateFrameNumbers("ship2"),
+      frameRate: 20,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "ship3_anim",
+      frames: this.anims.generateFrameNumbers("ship3"),
+      frameRate: 20,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "explode",
+      frames: this.anims.generateFrameNumbers("explosion"),
+      frameRate: 20,
+      repeat: 0,
+      hideOnComplete: true,
+    });
+
+    this.ship1.play("ship1_anim");
+    this.ship2.play("ship2_anim");
+    this.ship3.play("ship3_anim");
 
     this.add.text(20, 20, "Playing game", {
       font: "25px Arial",
